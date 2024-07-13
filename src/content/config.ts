a5d2334237ca40aceed4d49e650e9af368868f6b
timophey785 <85181478+timophey785@users.noreply.github.com>
2024-07-13 11:55:14 +0300

    Initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,8 @@
+// import { defineCollection } from "astro:content";
+
+// const collection = defineCollection({
+//   type: "data" | "content",
+//   schema: , // z-schema
+// });
+
+export const collections = {};
